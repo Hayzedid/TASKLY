aa0824510a9f817600a36734ca3d041f9579577e
Hayzedid <iazeez775@gmail.com>
2025-04-04 11:43:38 +0100

    added a delete button

--- App.tsx
+++ App.tsx
@@ -1,11 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View , Alert,TouchableOpacity } from 'react-native';
 
 export default function App() {
+  const handleDelete = () => {
+    Alert.alert(
+     "Are you sure you want to delete this ?",
+      "This action cannot be undone.",
+      [
+        { text: "Cancel", onPress: () => console.log("Cancel Pressed"), style: "cancel" },
+        { text: "OK", onPress: () => console.log("OK Pressed") }
+      ]
+  
+    );
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.itemContainer} >
+      <Text style={styles.itemText}>hello </Text>
+        <TouchableOpacity onPress={handleDelete} activeOpacity={0.8}  style={styles.button}>
+          <Text style={styles.buttonText}> Delete </Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -13,8 +28,35 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#f6f6f6',
     justifyContent: 'center',
   },
+  itemContainer: {
+    backgroundColor: '#5C8C46',
+        borderBottomColor: '#1a759f',
+        borderBottomWidth: 1,
+        paddingHorizontal: 8,
+        paddingVertical: 16,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        borderRadius: 8,
+        marginVertical: 4,
+        marginHorizontal: 16,
+  },
+  itemText:{
+    fontSize: 18, 
+    fontWeight: "200"
+  },
+  button: {
+    backgroundColor: "#0B2559",
+    borderRadius: 8,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 18,
+    fontWeight: "600",
+    padding: 10,
+    borderRadius: 8,
+  }
 });
---
+++ theme.ts
@@ -0,0 +1,8 @@
+export const theme = {   
+        // Define your theme colors here
+        primary: '#1a759f',
+        secondary: '#f6f6f6',
+        coloWhite: '#fff',
+        colorBlack: '00000'
+        
+   }
